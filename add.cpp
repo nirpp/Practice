de993e3cfd125a3b8d0cdb567cad2b99d26526ec
Nirzar <nirzarpanvelkar38@gmail.com>
2021-09-10 11:32:55 +0530

    first commit

---
+++ add.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{ int a,b,sum;
+    cout<<"enter the no. a="<<endl;
+     cin>>a;
+    cout<<"enter no. b="<<endl;
+      cin>>b;
+    sum=a+b;
+    cout<<"the sum is"<<sum;
+   
+  
+    return 0;
+}
+   
+    
+    
